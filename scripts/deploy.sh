b6a2f0bb3cd8335b28cbe897eb8d1542516dc892
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2020-11-02 11:01:31 +0100

    deploy script

---
+++ scripts/deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# deployment script for automated updates via github actions
+
+# get latest repo version
+git pull
+
+# pull latest versions of services
+docker-compose pull auth-proxy beerchat-proxy mapserver wiki highscore webmail manager
+
+# update deployment
+docker-compose up -d
+
+# cleanup old/stale images
+docker image prune -af
