fa71a4d85518ac636ccb7e65a75e946a187eee9c
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2020-11-22 09:54:30 +0100

    public beerchat config

---
+++ data/beerchat.js
@@ -0,0 +1,23 @@
+module.exports = {
+	"remotes": [{
+		"name": "IRC",
+		"type": "irc",
+		"host": "chat.freenode.net",
+		"username": "damoclesbot",
+		"password": process.env.BEERCHAT_IRC_PASSWORD,
+		"debug": false,
+		"system_channel": "main",
+		"channels": {
+			"main": "damocles"
+		}
+	},{
+		"name": "Discord",
+		"type": "discord",
+		"debug": false,
+		"token": process.env.BEERCHAT_DISCORD_TOKEN,
+		"system_channel": "main",
+		"channels": {
+			"main": "main"
+		}
+	}]
+};
